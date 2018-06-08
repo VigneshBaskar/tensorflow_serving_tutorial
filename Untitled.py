5207f1298438b36161be343326c185d86a805edc
Vignesh Baskar <vignesh.sbaskaran@gmail.com>
2018-06-08 15:47:02 +0200

    Added additional model and the configuration file working

--- Untitled.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# <nbformat>4</nbformat>
--- model_client.py
+++ model_client.py
@@ -7,12 +7,13 @@ from predict_client.prod_client import ProdClient
 
 # <codecell>
 
-client = ProdClient('localhost:9000', 'model_add', 4)
+client_1 = ProdClient('localhost:9000', 'model_sub', 1)
+client_2 = ProdClient('localhost:9000', 'model_add', 4)
 
 # <codecell>
 
 req_data = [{'in_tensor_name': 'input', 'in_tensor_dtype': 'DT_INT32', 'data': 5}]
+print(client_1.predict(req_data))
 
-# <codecell>
-
-client.predict(req_data)
+req_data = [{'in_tensor_name': 'input', 'in_tensor_dtype': 'DT_INT32', 'data': 5}]
+print(client_2.predict(req_data))
